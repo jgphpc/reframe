f02bb6eab533a36c2776e08ef2bdc9c40d9dbcf0
jgp <jgp@cscs.ch>
2020-06-17 18:46:47 +0200

    testing deps

---
+++ UES-914/0.py
@@ -0,0 +1,38 @@
+import os
+import reframe as rfm
+import reframe.utility.sanity as sn
+
+
+class TestBase(rfm.RunOnlyRegressionTest):
+    def __init__(self, myarg1):
+        self.valid_systems = ['dom:mc']
+        self.valid_prog_environs = ['*']
+        self.num_tasks = 1
+        self.time_limit = '1m'
+        self.executable = 'echo 1 step=%s' % myarg1
+        self.sanity_patterns = sn.all([sn.assert_found('1', self.stdout)])
+
+@rfm.parameterized_test(*[[steps]
+                          for steps in [1, 2]
+                          ])
+class MPI_ComputeTest(TestBase):
+    def __init__(self, steps):
+        super().__init__(steps)
+
+@rfm.simple_test
+class MPI_PostprocTest(rfm.RunOnlyRegressionTest):
+    def __init__(self):
+        self.valid_systems = ['dom:mc']
+        self.valid_prog_environs = ['*']
+        self.num_tasks_per_node = 1
+        self.num_tasks = 1
+        self.executable = 'echo 0'
+        # self.depends_on('ComputeTest_1')
+        self.depends_on('MPI_ComputeTest_2')
+        self.sanity_patterns = sn.assert_found(r'^\d+', self.stdout)
+
+    @rfm.require_deps
+    def collect_logs(self, MPI_ComputeTest):
+        mydirname = 'MPI_ComputeTest_*'
+        self.postrun_cmds = ['ls ../%s/*job.out' % mydirname]
+        # self.postrun_cmds = ['echo "%s"' % os.path.join(MPI_ComputeTest().stagedir)]
